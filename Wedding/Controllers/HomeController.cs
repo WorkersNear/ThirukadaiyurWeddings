662a07fc8fcf752a53fcde84bb1fbfc205c10546
Balamurugan <balamr491@gmail.com>
2018-11-26 20:14:17 +0530

    changes by bala

--- Wedding/Controllers/HomeController.cs
+++ Wedding/Controllers/HomeController.cs
@@ -41,28 +41,26 @@ namespace Wedding.Controllers
         [HttpPost]
         public ActionResult OnlineBooking(OnlineBooking onlineBooking)
         {
-            //SendEmailtoContacts();
+
             var fromAddress = new MailAddress("bala1991.phoenix@gmail.com", "New_Booking" + DateTime.Now);
             var toAddress = new MailAddress("abirami.arangements@gmail.com", "Abirami_Arrangements");
             const string fromPassword = "X3haquc6";
             const string subject = "New_Booking:";
-            // const string body = "test";
-            string body = "test";
 
-            string data = (" MrName:" + onlineBooking.MrName +
-            " MrsName:" + onlineBooking.MrsName +
-            " MrRasiandNakshatra:" + onlineBooking.MrRasiandNakshatra +
-            " MrsRasiandNakshatra :" + onlineBooking.MrsRasiandNakshatra +
-            " MobileNumber: " + onlineBooking.MobileNumber +
-            " AlterneteMobile: " + onlineBooking.AlterneteMobile +
-            " FunctionDate :" + onlineBooking.FunctionDate +
-            " Location :" + onlineBooking.Location +
-            " TypeofAbishegam : " + onlineBooking.TypeofAbishegam +
-            " AbishegamDays :" + onlineBooking.AbishegamDays +
-            " Bhramin : " + onlineBooking.Bhramin +
-            " Food :" + onlineBooking.Food +
-            " Hotel :" + onlineBooking.Hotel +
-            " Photography :" + onlineBooking.Photography +
+            string body = (" MrName:" + onlineBooking.MrName + "\n" +
+            " MrsName:" + onlineBooking.MrsName + "\n" +
+            " MrRasiandNakshatra:" + onlineBooking.MrRasiandNakshatra + "\n" +
+            " MrsRasiandNakshatra :" + onlineBooking.MrsRasiandNakshatra + "\n" +
+            " MobileNumber: " + onlineBooking.MobileNumber + "\n" +
+            " AlterneteMobile: " + onlineBooking.AlterneteMobile + "\n" +
+            " FunctionDate :" + onlineBooking.FunctionDate + "\n" +
+            " Location :" + onlineBooking.Location + "\n" +
+            " TypeofAbishegam : " + onlineBooking.TypeofAbishegam + "\n" +
+            " AbishegamDays :" + onlineBooking.AbishegamDays + "\n" +
+            " Bhramin : " + onlineBooking.Bhramin + "\n" +
+            " Food :" + onlineBooking.Food + "\n" +
+            " Hotel :" + onlineBooking.Hotel + "\n" +
+            " Photography :" + onlineBooking.Photography + "\n" +
             " VideoCoverage: " + onlineBooking.VideoCoverage);
 
             var smtp = new SmtpClient
@@ -80,47 +78,15 @@ namespace Wedding.Controllers
                 Body = body
             })
             {
-                smtp.Send(message);
+                //smtp.Send(message);
             }
-            return View();
+            return Json("OK", JsonRequestBehavior.AllowGet);
         }
 
         public ActionResult OnlineBookingconfirmation()
         {
             return View();
         }
-        private void SendEmailtoContacts()
-        {
-            string subjectEmail = "Meeting has been rescheduled.";
-            string bodyEmail = "Meeting is one hour later.";
-            Outlook.Application app = new Outlook.Application();
-            Outlook.MAPIFolder sentContacts = (Outlook.MAPIFolder)
-                app.ActiveExplorer().Session.GetDefaultFolder
-                (Outlook.OlDefaultFolders.olFolderContacts);
-            foreach (Outlook.ContactItem contact in sentContacts.Items)
-            {
-                if (contact.Email1Address.Contains("abirami.arangements@gmail.com"))
-                {
-                    this.CreateEmailItem(subjectEmail, contact
-                        .Email1Address, bodyEmail);
-                }
-            }
-        }
-
-        private void CreateEmailItem(string subjectEmail,
-               string toEmail, string bodyEmail)
-        {            
-
-            Outlook.Application app = new Outlook.Application();
-            Outlook.MailItem eMail = app.CreateItem(Outlook.OlItemType.olMailItem);
-            eMail.Subject = subjectEmail;
-            eMail.To = toEmail;
-            eMail.Body = bodyEmail;
-            eMail.Importance = Outlook.OlImportance.olImportanceLow;
-            ((Outlook._MailItem)eMail).Send();
-        }
-
-
 
     }
 }
